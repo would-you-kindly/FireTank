88bc2924ae7273b0ef703921c54229371e968467
would-you-kindly <would.you.kindly@yandex.ru>
2018-05-16 13:31:11 +0500

    Добавил эффект синергии (пока с ошибкой)

--- src/States/BurningHouseState.cs
+++ src/States/BurningHouseState.cs
@@ -11,13 +11,24 @@ namespace FireSafety
         public int needShootsToExtinguishCount = 3;
         public int currentShootsToExtinguishCount = 0;
 
+        private static int algorithmActionNumber = -1;
+
         public BurningHouseState()
         {
             currentShootsToExtinguishCount = 0;
+            algorithmActionNumber = -1;
         }
 
         public override void Extinguish(House house)
         {
+            // Проверяем, был ли еще один выстрел на том же шаге алгоритма (эффект синергии)
+            if (ParallelAlgorithm.GetInstance().currentAction == algorithmActionNumber)
+            {
+                currentShootsToExtinguishCount += 2;
+            }
+
+            algorithmActionNumber = ParallelAlgorithm.GetInstance().currentAction;
+
             if (++currentShootsToExtinguishCount >= needShootsToExtinguishCount)
             {
                 house.state = new NormalHouseState();
